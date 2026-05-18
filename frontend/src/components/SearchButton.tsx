44d4709dfd1df6bae29923fa8927e83b39a7f33a
hisa <gwall59@gmail.com>
2026-05-19 06:25:38 +0900

    検索ボタンのサイズを距離セレクターのサイズに合わせる

--- frontend/src/components/SearchButton.tsx
+++ frontend/src/components/SearchButton.tsx
@@ -1,18 +1,17 @@
 import Button from 'react-bootstrap/Button';
+import './SearchButton.css'
 
 type SearchButtonProps = {
     onSearch: () => void;
     radius: number;
 };
-
 export const SearchButton = (props:SearchButtonProps) => {
     const {onSearch, radius} = props;
  return (
      <Button
          variant="primary"
-         className="mb-3"
+         className="mb-3 search-button"
          onClick={onSearch}
-         style={{ display: 'block', margin: '20px auto' }}
      >
          {radius / 1000}km以内で探す
      </Button>
