6b678389d0736db984c506aaac0f40955c64cf77
hisa <gwall59@gmail.com>
2026-05-19 21:35:58 +0900

    InfowWindowのpadding-topを調整

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -100,7 +100,7 @@ const Map = () => {
                         >
                             <Stack gap={2}>
                                 <div className="close-button-area">
-                                    <h3 className="fw-bold mt-3">
+                                    <h3 className="fw-bold">
                                         {selectedShop.name}
                                     </h3>
                                     <Button className="close-button"
